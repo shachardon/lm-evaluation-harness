9dacc4449091a9b3db22349f266faed832397c10
Yotam-Perlitz <y.perlitz@ibm.com>
2024-10-28 19:28:54 +0100

    add task hack to use local unitxt
    Signed-off-by: Yotam-Perlitz <y.perlitz@ibm.com>

--- lm_eval/api/task.py
+++ lm_eval/api/task.py
@@ -22,6 +22,7 @@ from typing import (
 
 import datasets
 import numpy as np
+import unitxt
 from tqdm import tqdm
 
 from lm_eval import utils
@@ -263,13 +264,24 @@ class Task(abc.ABC):
             - `datasets.DownloadMode.FORCE_REDOWNLOAD`
                 Fresh download and fresh dataset.
         """
-        self.dataset = datasets.load_dataset(
-            path=self.DATASET_PATH,
-            name=self.DATASET_NAME,
-            data_dir=data_dir,
-            cache_dir=cache_dir,
-            download_mode=download_mode,
-        )
+        # self.dataset = datasets.load_dataset(
+        #     path=self.DATASET_PATH,
+        #     name=self.DATASET_NAME,
+        #     data_dir=data_dir,
+        #     cache_dir=cache_dir,
+        #     download_mode=download_mode,
+        # )
+
+        if self.DATASET_PATH == "unitxt/data":
+            self.dataset = unitxt.load_dataset(self.DATASET_NAME)
+        else:
+            self.dataset = datasets.load_dataset(
+                path=self.DATASET_PATH,
+                name=self.DATASET_NAME,
+                data_dir=data_dir,
+                cache_dir=cache_dir,
+                download_mode=download_mode,
+            )
 
     @property
     def config(self) -> TaskConfig:
